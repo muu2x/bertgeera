13a240795efc289b5a5de1bdb83d4b61dd328a11
momo <44685663+muu2x@users.noreply.github.com>
2026-01-29 00:03:37 -0500

    buck it we fall

--- assets/js/xxx.in.js
+++ assets/js/xxx.in.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use strict';
 
 $(window).on('load', function () {});
@@ -29,36 +28,4 @@ var In = {
     this.resize();
   }
 };
-=======
-'use strict';
-
-$(window).on('load', function () {});
-
-(function () {
-  setTimeout(function () {
-    $('.js-wrap').addClass('is-in');
-  }, 100);
-})();
-
-var In = {
-  conf: {
-    elem: '.js-in'
-  },
-  set: function set() {
-    var setHeight = $(window).innerHeight() * .8;
-    $(this.conf.elem).css({ 'min-height': setHeight });
-  },
-  resize: function resize() {
-    var _this = this;
-
-    $(window).on('resize', function () {
-      _this.set();
-    });
-  },
-  default: function _default() {
-    this.set();
-    this.resize();
-  }
-};
->>>>>>> 7d6d769e7d4d6c28d5b4cdd0122f1a22687c4a56
 In.default();
