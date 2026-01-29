3063f60f33cfd20f375d518beaaccef2b20fd01f
momo <44685663+muu2x@users.noreply.github.com>
2026-01-29 00:26:57 -0500

    Fixed Vender Scripts

--- assets/js/vender/aip.setting.js
+++ assets/js/vender/aip.setting.js
@@ -1,12 +1,4 @@
-<<<<<<< HEAD
 var _artistFolder = '';
 var _artistId = '';
 
 
-=======
-
-var _artistFolder = 'kaguyaluna';
-var _artistId = '70009300';
-
-
->>>>>>> 7d6d769e7d4d6c28d5b4cdd0122f1a22687c4a56
